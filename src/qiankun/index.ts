5bd22260c8d6c3d052c7eef05f8d9e7704380d91
卢杰文 <lu_jiewen@gov-info.cn>
2022-12-11 22:52:26 +0800

    init

--- src/qiankun/index.ts
+++ src/qiankun/index.ts
@@ -4,7 +4,7 @@ import * as actions from './shared'
 registerMicroApps([
     {
       name: 'subReportAdmin',
-      entry: '//localhost:8090',
+      entry: '//huahill-nj:8090',
       container: '#container',
       activeRule: '/sub-report-admin',
       props: { actions },
